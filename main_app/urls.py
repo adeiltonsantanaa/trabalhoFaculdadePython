678eaec5b6815cc1e5adcf648ef3572645cb0065
GabrielFigueiredoL <gabrieellfigueiredo@gmail.com>
2023-06-02 04:39:52 -0300

    path da função verificar_url

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('enderecos/', views.lista_enderecos, name='lista_enderecos'),
     path('enderecos/excluir/<int:pk>/', views.excluir_endereco, name='excluir_endereco'),
     path('alterar/<int:pk>/', views.alterar_url, name='alterar_url'),
+    path('verificar_url/<int:pk>', views.verificar_url, name='verificar_url')
 ]
